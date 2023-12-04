d38ff41822e7c754f368123b0577238025a6428d
Tiago <trsminfo@gmail.com>
2023-12-04 11:55:35 -0300

    inicio backend

---
+++ backend/src/route.ts
@@ -0,0 +1,11 @@
+import { Router, Request, Response } from 'express'
+
+
+
+const router = Router();
+
+router.get('/teste', (req: Request, res: Response) => {
+    return res.json({ nome: "teste Pizzaria rota get" })
+
+})
+export { router };
---
+++ backend/src/server.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors'
+
+import { router } from './route';
+
+const app = express();
+app.use(express.json())
+
+app.use(router)
+
+
+
+// middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return res.status(400).json({
+            error: err.message
+        })
+    }
+
+    return res.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    })
+})
+
+app.listen(3333, () => {
+    console.log('Server is running')
+})
